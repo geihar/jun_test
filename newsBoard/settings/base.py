2ac9c6c281480c0fdae70744fefc294037a2f9ce
geihar <l0635968488@gmail.com>
2020-05-17 17:31:00 +0200

    fixed bug with authorization

--- newsBoard/settings/base.py
+++ newsBoard/settings/base.py
@@ -134,10 +134,12 @@ STATIC_URL = "/static/"
 
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.TokenAuthentication",
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+),
 }
 
 SIMPLE_JWT = {
